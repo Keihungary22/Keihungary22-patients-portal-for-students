9360823d3656988a90a89dd4af7a2254930ac4c1
tezukakei <147504160+Keihungary22@users.noreply.github.com>
2024-09-16 17:06:28 +0200

    Initial commit

--- src/api_controller.py
+++ src/api_controller.py
@@ -1,6 +1,6 @@
 """Patient API Controller"""
 
-from flask import Flask
+from flask import Flask, jsonify, request
 from patient_db import PatientDB
 
 
@@ -15,6 +15,7 @@ class PatientAPIController:
         """
         Sets up the routes for the API endpoints.
         """
+        self.app.route("/", methods=["GET"])(self.home)
         self.app.route("/patients", methods=["GET"])(self.get_patients)
         self.app.route("/patients/<patient_id>", methods=["GET"])(self.get_patient)
         self.app.route("/patients", methods=["POST"])(self.create_patient)
@@ -33,25 +34,64 @@ class PatientAPIController:
     """
 
     def create_patient(self):
-        pass
+        try:
+            data = request.json
+            required_fields = ['patient_name', 'patient_gender', 'patient_age']
+            if all(field in data for field in required_fields):
+                # patient = self.patient_db.insert_patient(data)
+                patient_id = self.patient_db.insert_patient(data)
+                if patient_id:
+                    return jsonify({"message": "Patient created successfully", "patient_id": str(patient_id)}), 200
+                else:
+                    return jsonify({"message": "Failed to create patient"}), 400
+            else: 
+                return jsonify({"message": "Missing required fields"}), 400
+        except Exception as e:
+            return jsonify({"message": str(e)}), 400
 
     def get_patients(self):
-        pass
+        patients = self.patient_db.select_all_patients()
+        if patients:
+            return jsonify(patients), 200
+        else:
+            return jsonify({"message": "Patient not found"}), 400
 
     def get_patient(self, patient_id):
-        pass
+        patient = self.patient_db.select_patient(patient_id)
+        if patient:
+            return jsonify(patient), 200
+        else:
+            return jsonify({"message": "Patient not found"}), 400
 
     def update_patient(self, patient_id):
-        pass
+        try:
+            data = request.json
+            if data:
+                patient = self.patient_db.update_patient(patient_id, data)
+                if patient:
+                    return jsonify({"message": "Patient updated successfully"}), 200
+                else:
+                    return jsonify({"message": "Failed to update patient"}), 400
+            else:
+                return jsonify({"message": "No data provided"}), 400
+        except Exception as e:
+            return jsonify({"message": str(e)}), 400
 
     def delete_patient(self, patient_id):
-        pass
+        patient = self.patient_db.delete_patient(patient_id)
+        if patient:
+            return jsonify({"message": "Patient deleted successfully"}), 200
+        else:
+            return jsonify({"message": "Failed to delete patient"}), 400
 
     def run(self):
         """
         Runs the Flask application.
         """
-        self.app.run()
+        self.app.run(debug=True)
+
+    def home(self):
+        return jsonify({"message": "Welcomem to the Patient API"}), 200
 
 
 PatientAPIController()
--- src/patient.py
+++ src/patient.py
@@ -16,3 +16,49 @@ Validation should be done for all of the variables in config and db_config.
 
 There should be a method to commit that patient to the database using the api_controller.
 """
+
+import uuid
+from datetime import datetime
+from config import GENDERS, WARD__NUMBERS, ROOM_NUMBERS
+from patient_db_config import PATIENT_COLUMN_NAMES
+from api_controller import PatientAPIController
+
+class Patient:
+    def __init__(self, name, gender, age):
+        self.id = str(uuid.uuid4())
+        self.gender = gender
+        self.age = age
+        self.checkin = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        self.checkout = None
+        self.ward = None
+        self.room = None
+
+    def update_room_and_ward(self, ward, room):
+        if ward in WARD_NUMBERS and room in ROOM_NUMBERS[ward]:
+            self.ward = ward
+            self.room = room
+            return True
+        else:
+            print("Invalid ward or room number.")
+            return False
+
+    def commit_to_database(self):
+        api_controller = PatientAPIController()
+        patient_data = {
+            "patient_id": self.id,
+            "patiend_name": self.name,
+            "patient_gender": self.gender,
+            "patient_age": self.age,
+            "patient_checkin": self.checkin,
+            "patient_checkout": self.checkout,
+            "patient_ward": self.ward,
+            "patient_room": self.room
+        }
+        response = api_controller.create_patient(patient_data)
+        if response.status_code == 200:
+            print("Patient committed to the database successfully.")
+            return True
+        else:
+            print("Failed to commit patient to the database.")
+            return False
+    
